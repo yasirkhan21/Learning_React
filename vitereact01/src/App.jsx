e58a47bef977bcd001f9fd9bc71d7ebcaa3aec54
Yasir khan <yk1809786@gmail.com>
2024-02-14 13:42:03 +0530

    react using vite

---
+++ vitereact01/src/App.jsx
@@ -0,0 +1,13 @@
+import Tea from "./Tea";
+function App() {
+  return (
+<> 
+<Tea/>  
+<h1>React</h1>
+<p>Test</p>
+</>
+
+  );
+}
+
+export default App
---
+++ vitereact01/src/Tea.jsx
@@ -0,0 +1,6 @@
+function Tea(){
+    return(
+    <h1>Tea is Ready!!</h1>
+    )
+}
+export default Tea
